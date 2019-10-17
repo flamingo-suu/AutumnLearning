966f8c19f079f1c1038b4f9fe38d8a5788e039f4
flamingo-suu <flamingo.suu@gmail.com>
2019-10-17 11:21:11 -0700

    Edited

--- src/com/flamingosoft/Bai4.java
+++ src/com/flamingosoft/Bai4.java
@@ -1,7 +1,41 @@
 package com.flamingosoft;
 
-public class Bai4 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Bai4 {
+    private static int n;
+    private static int[] a;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nTest = scanner.nextInt();
+        for (int i = 0; i < nTest; i++) {
+            n = scanner.nextInt();
+            a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = scanner.nextInt();
+            }
+            System.out.println("Test " + (i+1) + ":");
+            countArrays();
+        }
+        scanner.close();
+    }
+
+    private static void countArrays() {
+        int[] b = new int[n];
+        int[] dem = new int[100001];
+        int tmp = 0;
+        for (int value : a) {
+            for (int j = 0; j <= tmp; j++) {
+                if (value == b[j]) {
+                    dem[b[j]]++;
+                }
+            }
+            b[tmp] = value;
+            dem[value]++;
+            tmp++;
+        }
+        for (int i = 0; i < tmp; i++) {
+            System.out.println(b[i] +"  " + dem[b[i]]);
+        }
     }
 }
--- src/com/flamingosoft/Bai6.java
+++ src/com/flamingosoft/Bai6.java
@@ -1,7 +1,41 @@
 package com.flamingosoft;
 
+import java.util.Scanner;
+
 public class Bai6 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int nTest = scanner.nextInt();
+        for (int i = 0; i < nTest; i++) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int[][] a = new int[n][m];
+            int[][] b = new int[m][n];
+            int[][] res = new int[n][n];
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    a[j][k] = scanner.nextInt();
+                }
+            }
+            for (int j = 0; j <n ; j++) {
+                for (int k = 0; k < m; k++) {
+                    b[k][j] = a[j][k];
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < m; l++) {
+                        res[j][k] = res[j][k]+ a[j][l]*b[l][k];
+                    }
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    System.out.print(res[j][k] +" ");
+                }
+                System.out.println();
+            }
+        }
+        scanner.close();
     }
 }
