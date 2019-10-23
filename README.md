a0164b8cce7c02624d3cf1fb70ccf4da69c91540
flamingo-suu <flamingo.suu@gmail.com>
2019-10-23 04:05:22 -0700

    Edited

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 AutumnLearning
 
-# Test done
+# All test have done
 
 ## One
+
+- Bai1
+- Bai2
+- Bai3
+- Bai4
+- Bai5
+- Bai6
+- Bai7
+- Bai8
+- Bai9
+- Bai10
+- Bai11
+- Bai12
+- Bai13
+- Bai
+
+
 
 ## Two
 - Bai1
--- src/com/flamingosoft/two/Bai7.java
+++ src/com/flamingosoft/two/Bai7.java
@@ -1,7 +1,26 @@
 package com.flamingosoft.two;
 
+import java.util.Scanner;
+
 public class Bai7 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nTest = scanner.nextInt();
+        for (int i = 0; i < nTest; i++) {
+            int n= scanner.nextInt();
+            int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = scanner.nextInt();
+            }
+            int l = 0;
+            int r = n-1;
 
+            if (l ==r){
+                System.out.println("YES");
+            }else {
+                System.out.println(r-l+1);
+            }
+        }
+        scanner.close();
     }
 }
